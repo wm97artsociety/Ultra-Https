8a296a2c2459a4d53f7fb9277663a6e33d403cfb
wm97artsociety <wm97artsociety@gmail.com>
2025-05-19 01:48:53 -0400

    Delete Content_script.js

--- Content_script.js
+++
@@ -1,8 +0,0 @@
-import { initEncryptionAddon } from "./plugin_api.js";
-
-(function() {
-  // Inject encryption challenge UI & logic into the current page
-  initEncryptionAddon();
-
-  // You can add listeners here to communicate with background if needed
-})();
