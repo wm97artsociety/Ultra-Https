d3639f8d90a82e5b3e7495bf233d03c6ee96035b
wm97artsociety <wm97artsociety@gmail.com>
2025-05-19 01:27:31 -0400

    Create Content_script.js

---
+++ Content_script.js
@@ -0,0 +1,8 @@
+import { initEncryptionAddon } from "./plugin_api.js";
+
+(function() {
+  // Inject encryption challenge UI & logic into the current page
+  initEncryptionAddon();
+
+  // You can add listeners here to communicate with background if needed
+})();
